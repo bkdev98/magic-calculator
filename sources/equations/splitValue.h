ab9ff1de2eda4483f238bb2ea40197ccfff1dfb7
bkdev98 <bkdev98@gmail.com>
2017-01-03 19:39:17 +0700

    Added splitTwoThreeHiddensEquation

--- sources/equations/splitValue.h
+++ sources/equations/splitValue.h
@@ -279,270 +279,381 @@ void splitValueCubicEquation(char str[]) {
   d0[j] = '\0';
 }
 
-void splitValueTwoHiddensEquation(char s[])
-{
-    char s1[100], s2[100];
-
-    fflush(stdin);
-    {
-        int i = 0, j = 0;
-        for (i = 0; s[i] != '&'; i++)
-            s1[i] = s[i];
-        s1[i] = '\0';
-        i++;
-        for (j = 0 ; i < strlen(s); i++, j++)
-            s2[j] = s[i];
-        s2[j] = '\0';
-
-    }
-
-
-    char tmp1[100], tmp2[100], tmp3[100], tmp4[100];
-
-    {
-        fflush(stdin);
-        int i = 0, j = 0;
-        for (i = 0; s1[i] != '='; i++)
-            tmp1[i] = s1[i];
-        tmp1[i] = '\0';
-
-        for (j = 0 ; i < strlen(s1); i++, j++)
-            tmp2[j] = s1[i];
-        tmp2[j] = '\0';
-
-    }
-
-    {
-        fflush(stdin);
-        int i = 0, j = 0;
-        for (i = 0; s2[i] != '='; i++)
-            tmp3[i] = s2[i];
-        tmp3[i] = '\0';
-
-        for (j = 0 ; i < strlen(s2); i++, j++)
-            tmp4[j] = s2[i];
-        tmp4[j] = '\0';
-
-    }
-
-
-    int check = 0;
-    {
-        fflush(stdin);
-        char tmpA[100], tmpB[100], tmpC[100];
-        int i = 0, j = 0;
-        while (tmp1[i] != 'x' && tmp1[i] != 'y' && i < strlen(tmp1)) {
-            tmpA[j] = tmp1[i];
-            i++;
-            j++;
-        }
-        tmpA[j] = '\0';
-        if ( j == 0 && tmp1[i] == 'x')
-            strcpy(a0, "1");
-        else if ( j == 1 && tmp1[i] == 'x'&& tmpA[0] == '-'){
-
-                strcpy(a0, "-1");
-
-        }
-        else if ( j == 1 && tmp1[i] == 'x'&& tmpA[0] == '+')
-            strcpy(a0, "1");
-        else if ( tmp1[i] == 'x' ){
-            strcpy(a0, tmpA);
-        }
-        else if ( j == 0 && tmp1[i] == 'y'){
-            strcpy(b0, "1");
-        }
-        else if (  j == 1 && tmp1[i] == 'y'&& tmpA[0] == '-'){
-
-                strcpy(b0, "-1");
-
-        }
-        else if ( j == 1 && tmp1[i] == 'y'&& tmpA[0] == '+')
-            strcpy(b0, "1");
-        else if ( tmp1[i] == 'y' ){
-            strcpy(b0, tmpA);
-        }
-
-        fflush(stdin);
-        i++;
-        j = 0;
-        while (tmp1[i] != 'x' && tmp1[i] != 'y' && i < strlen(tmp1)) {
-            tmpB[j] = tmp1[i];
-            i++;
-            j++;
-        }
-        tmpB[j] = '\0';
-        if ( j == 1 && tmp1[i] == 'x'){
-            strcpy(a0, tmpB);
-            a0[1] = '1';
-        }
-        else if ( tmp1[i] == 'x' ){
-            strcpy(a0, tmpB);
-        }
-        else if ( j == 1 && tmp1[i] == 'y'){
-            strcpy(b0, tmpB);
-            b0[1] = '1';
-        }
-        else if ( tmp1[i] == 'y' ){
-            strcpy(b0, tmpB);
-        }
-
-        fflush(stdin);
-        if ( (i + 1) < strlen(tmp1)){
-            i++;
-            j = 0;
-            while (i < strlen(tmp1)){
-                tmpC[j] = tmp1[i];
-                i++;
-                j++;
-            }
-            tmpC[j] = '\0';
-            strcpy(c0, tmpC);
-            check = 1;
-
-        }
-    }
-
-//==============================================================================
-
-    {
-        fflush(stdin);
-        char tmpD[100];
-        if (check == 0){
-
-            char tmpD[100];
-
-            {
-                int i;
-                for(i = 0; i < (strlen(tmp2) - 1); i++)
-                    tmpD[i] = tmp2[i + 1];
-                tmpD[i] = '\0';
-            }
-            if ( tmpD[0] == '-')
-                for (int t = 1; t < strlen (tmp2); t++)
-                    c0[t - 1] = tmpD[t];
-            else if (tmpD[0] == '+'){
-                c0[0] = '-';
-                for (int t = 1; t < strlen (tmp2); t++)
-                        c0[t] = tmpD[t];
-            }
-            else{
-                c0[0] = '-';
-                for (int t = 0; t < strlen (tmp2); t++)
-                    c0[t + 1] = tmpD[t];
-            }
-        }
-    }
-
-
-//==============================================================================
-
-{
-    fflush(stdin);
-    check = 0;
-    char tmpE[100], tmpF[100], tmpG[100];
-    int i = 0, j = 0;
-
-    while (tmp3[i] != 'x' && tmp3[i] != 'y' && i < strlen(tmp3)) {
-        tmpE[j] = tmp3[i];
-        i++;
-        j++;
-    }
-    tmpE[j] = '\0';
-
-    if ( j == 0 && tmp3[i] == 'x')
-        strcpy(a1, "1");
-    else if ( j == 1 && tmp3[i] == 'x' && tmpE[0] == '-'){
-            strcpy(a1, "-1");
-    }
-    else if ( j == 1 && tmp3[i] == 'x' && tmpE[0] == '+')
-        strcpy(a1, "1");
-    else if ( tmp3[i] == 'x' ){
-        strcpy(a1, tmpE);
-    }
-    else if ( j == 0 && tmp3[i] == 'y'){
-        strcpy(b1, "1");
-    }
-    else if (  j == 1 && tmp3[i] == 'y' && tmpE[0] == '-'){
-
-            strcpy(b1, "-1");
-
-    }
-    else if (  j == 1 && tmp3[i] == 'y' && tmpE[0] == '+')
-        strcpy(a1, "1");
-    else if ( tmp3[i] == 'y' ){
-        strcpy(b1, tmpE);
-    }
-    fflush(stdin);
+void splitValueTwoHiddensEquation(char str[]) {
+  // -2x+3y-6=0&x-y=9
+  int i = 0, j = 0;
+  while (str[i] != 'x') {
+    a0[j] = str[i];
     i++;
-    j = 0;
-    while (tmp3[i] != 'x' && tmp3[i] != 'y' && i < strlen(tmp3)) {
-        tmpF[j] = tmp3[i];
+    j++;
+  }
+  if (j == 0) {
+    a0[0] = '1';
+    j = 1;
+  }
+  if (j == 1 && a0[0] == '-') {
+    a0[0] = '-';
+    a0[1] = '1';
+    j = 2;
+  }
+  if (j == 1 && a0[0] == '+') {
+    a0[0] = '+';
+    a0[1] = '1';
+    j = 2;
+  }
+  a0[j] = '\0';
+  ++i;
+  j = 0;
+  while (str[i] != 'y') {
+    b0[j] = str[i];
+    i++;
+    j++;
+  }
+  if (j == 1 && b0[0] == '-') {
+    b0[0] = '-';
+    b0[1] = '1';
+    j = 2;
+  }
+  if (j == 1 && b0[0] == '+') {
+    b0[0] = '+';
+    b0[1] = '1';
+    j = 2;
+  }
+  b0[j] = '\0';
+  ++i;
+  j = 0;
+  if (str[i] != '=') {
+    if (str[i] == '-') {
+      c0[j] = '+';
+      j++;
+      i++;
+      while (str[i] != '=') {
+        c0[j] = str[i];
         i++;
         j++;
-    }
-    tmpF[j] = '\0';
-    if ( j == 1 && tmp3[i] == 'x'){
-        strcpy(a1, tmpF);
-        a1[1] = '1';
-    }
-    else if ( tmp3[i] == 'x' ){
-        strcpy(a1, tmpF);
-    }
-    else if ( j == 1 && tmp3[i] == 'y'){
-        strcpy(b1, tmpF);
-        b1[1] = '1';
-    }
-    else if ( tmp3[i] == 'y' ){
-        strcpy(b1, tmpF);
-    }
-    fflush(stdin);
-    if ( (i+1) < strlen(tmp3)){
+      }
+      c0[j] = '\0';
+    } else if (str[i] =='+') {
+      c0[j] = '-';
+      j++;
+      i++;
+      while (str[i] != '=') {
+        c0[j] = str[i];
         i++;
-        j = 0;
-        while (i < strlen(tmp3)){
-            tmpG[j] = tmp3[i];
-            i++;
-            j++;
-        }
-        tmpG[j] = '\0';
-        strcpy(c1, tmpG);
-        check = 1;
-
+        j++;
+      }
+      c0[j] = '\0';
     }
+    i += 2;
+  } else {
+    i++;
+    while (str[i] != '&') {
+      c0[j] = str[i];
+      i++;
+      j++;
+    }
+    c0[j] = '\0';
+  }
+
+  ++i;
+  j = 0;
+  while (str[i] != 'x') {
+    a1[j] = str[i];
+    i++;
+    j++;
+  }
+  if (j == 0) {
+    a1[0] = '1';
+    j = 1;
+  }
+  if (j == 1 && a1[0] == '-') {
+    a1[0] = '-';
+    a1[1] = '1';
+    j = 2;
+  }
+  if (j == 1 && a1[0] == '+') {
+    a1[0] = '+';
+    a1[1] = '1';
+    j = 2;
+  }
+  a1[j] = '\0';
+  ++i;
+  j = 0;
+  while (str[i] != 'y') {
+    b1[j] = str[i];
+    i++;
+    j++;
+  }
+  if (j == 1 && b1[0] == '-') {
+    b1[0] = '-';
+    b1[1] = '1';
+    j = 2;
+  }
+  if (j == 1 && b1[0] == '+') {
+    b1[0] = '+';
+    b1[1] = '1';
+    j = 2;
+  }
+  b1[j] = '\0';
+  ++i;
+  j = 0;
+  if (str[i] != '=') {
+    if (str[i] == '-') {
+      c1[j] = '+';
+      j++;
+      i++;
+      while (str[i] != '=') {
+        c1[j] = str[i];
+        i++;
+        j++;
+      }
+      c1[j] = '\0';
+    } else if (str[i] =='+') {
+      c1[j] = '-';
+      j++;
+      i++;
+      while (str[i] != '=') {
+        c1[j] = str[i];
+        i++;
+        j++;
+      }
+      c1[j] = '\0';
+    }
+  } else {
+    i++;
+    while (str[i] != '\0') {
+      c1[j] = str[i];
+      i++;
+      j++;
+    }
+  }
+
+  c1[j] = '\0';
 }
 
-//==============================================================================
 
-    {
-        fflush(stdin);
-        char tmpH[100];
-
-        if (check == 0){
-            char tmpH[100];
-
-            fflush(stdin);
-            {
-                int i;
-                for(i = 0; i < (strlen(tmp4) - 1); i++)
-                    tmpH[i] = tmp4[i + 1];
-                tmpH[i] = '\0';
-            }
-
-            fflush(stdin);
-            if ( tmpH[0] == '-')
-                for (int t = 1; t < strlen (tmp4); t++)
-                    c1[t - 1] = tmpH[t];
-            else if (tmpH[0] == '+'){
-                c1[0] = '-';
-                for (int t = 1; t < strlen (tmp4); t++)
-                    c1[t] = tmpH[t];
-            }
-            else{
-                c1[0] = '-';
-                for (int t = 0; t < strlen (tmp4); t++)
-                    c1[t + 1] = tmpH[t];
-            }
-        }
+void splitValueThreeHiddensEquation(char str[]) {
+  // -2x+3y-6=0&x-y=9
+  int i = 0, j = 0;
+  while (str[i] != 'x') {
+    a0[j] = str[i];
+    i++;
+    j++;
+  }
+  if (j == 0) {
+    a0[0] = '1';
+    j = 1;
+  }
+  if (j == 1 && a0[0] == '-') {
+    a0[0] = '-';
+    a0[1] = '1';
+    j = 2;
+  }
+  if (j == 1 && a0[0] == '+') {
+    a0[0] = '+';
+    a0[1] = '1';
+    j = 2;
+  }
+  a0[j] = '\0';
+  ++i;
+  j = 0;
+  while (str[i] != 'y') {
+    b0[j] = str[i];
+    i++;
+    j++;
+  }
+  if (j == 1 && b0[0] == '-') {
+    b0[0] = '-';
+    b0[1] = '1';
+    j = 2;
+  }
+  if (j == 1 && b0[0] == '+') {
+    b0[0] = '+';
+    b0[1] = '1';
+    j = 2;
+  }
+  b0[j] = '\0';
+  ++i;
+  j = 0;
+  if (str[i] != '=') {
+    if (str[i] == '-') {
+      c0[j] = '+';
+      j++;
+      i++;
+      while (str[i] != '=') {
+        c0[j] = str[i];
+        i++;
+        j++;
+      }
+      c0[j] = '\0';
+    } else if (str[i] =='+') {
+      c0[j] = '-';
+      j++;
+      i++;
+      while (str[i] != '=') {
+        c0[j] = str[i];
+        i++;
+        j++;
+      }
+      c0[j] = '\0';
     }
+    i += 2;
+  } else {
+    i++;
+    while (str[i] != '&') {
+      c0[j] = str[i];
+      i++;
+      j++;
+    }
+    c0[j] = '\0';
+  }
+
+  ++i;
+  j = 0;
+  while (str[i] != 'x') {
+    a1[j] = str[i];
+    i++;
+    j++;
+  }
+  if (j == 0) {
+    a1[0] = '1';
+    j = 1;
+  }
+  if (j == 1 && a1[0] == '-') {
+    a1[0] = '-';
+    a1[1] = '1';
+    j = 2;
+  }
+  if (j == 1 && a1[0] == '+') {
+    a1[0] = '+';
+    a1[1] = '1';
+    j = 2;
+  }
+  a1[j] = '\0';
+  ++i;
+  j = 0;
+  while (str[i] != 'y') {
+    b1[j] = str[i];
+    i++;
+    j++;
+  }
+  if (j == 1 && b1[0] == '-') {
+    b1[0] = '-';
+    b1[1] = '1';
+    j = 2;
+  }
+  if (j == 1 && b1[0] == '+') {
+    b1[0] = '+';
+    b1[1] = '1';
+    j = 2;
+  }
+  b1[j] = '\0';
+  ++i;
+  j = 0;
+  if (str[i] != '=') {
+    if (str[i] == '-') {
+      c1[j] = '+';
+      j++;
+      i++;
+      while (str[i] != '=') {
+        c1[j] = str[i];
+        i++;
+        j++;
+      }
+      c1[j] = '\0';
+    } else if (str[i] =='+') {
+      c1[j] = '-';
+      j++;
+      i++;
+      while (str[i] != '=') {
+        c1[j] = str[i];
+        i++;
+        j++;
+      }
+      c1[j] = '\0';
+    }
+    i += 2;
+  } else {
+    i++;
+    while (str[i] != '&') {
+      c1[j] = str[i];
+      i++;
+      j++;
+    }
+    c1[j] = '\0';
+  }
+
+  ++i;
+  j = 0;
+  while (str[i] != 'x') {
+    a2[j] = str[i];
+    i++;
+    j++;
+  }
+  if (j == 0) {
+    a2[0] = '1';
+    j = 1;
+  }
+  if (j == 1 && a2[0] == '-') {
+    a2[0] = '-';
+    a2[1] = '1';
+    j = 2;
+  }
+  if (j == 1 && a2[0] == '+') {
+    a2[0] = '+';
+    a2[1] = '1';
+    j = 2;
+  }
+  a2[j] = '\0';
+  ++i;
+  j = 0;
+  while (str[i] != 'y') {
+    b2[j] = str[i];
+    i++;
+    j++;
+  }
+  if (j == 1 && b2[0] == '-') {
+    b2[0] = '-';
+    b2[1] = '1';
+    j = 2;
+  }
+  if (j == 1 && b2[0] == '+') {
+    b2[0] = '+';
+    b2[1] = '1';
+    j = 2;
+  }
+  b2[j] = '\0';
+  ++i;
+  j = 0;
+  if (str[i] != '=') {
+    if (str[i] == '-') {
+      c2[j] = '+';
+      j++;
+      i++;
+      while (str[i] != '=') {
+        c2[j] = str[i];
+        i++;
+        j++;
+      }
+      c2[j] = '\0';
+    } else if (str[i] =='+') {
+      c2[j] = '-';
+      j++;
+      i++;
+      while (str[i] != '=') {
+        c2[j] = str[i];
+        i++;
+        j++;
+      }
+      c2[j] = '\0';
+    }
+  } else {
+    i++;
+    while (str[i] != '\0') {
+      c2[j] = str[i];
+      i++;
+      j++;
+    }
+  }
+  c2[j] = '\0';
 }
